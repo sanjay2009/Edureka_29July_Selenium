92b93aa290694b3c70822cb37b0cc743cfb1f6e0
Sanjay <ramesh.sanjay@gmail.com>
2019-08-15 19:54:17 +0530

    first commit

---
+++ src/module2/BasicSeleniumCommands.java
@@ -0,0 +1,18 @@
+package module2;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BasicSeleniumCommands {
+
+	public static void main(String[] args) {
+		
+		//Initiate browser
+		System.setProperty("webdriver.chrome.driver", "D:\\Sanjay\\Edreka\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		driver.close();
+		
+	}
+
+}
